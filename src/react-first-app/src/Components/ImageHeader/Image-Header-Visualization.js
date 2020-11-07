d57013418d4e94fcea116af7347810769ed9e415
tripi <miguelangel.tripi@kapsch.net>
2020-11-07 17:05:34 -0300

    Adding alice carousel.

--- src/react-first-app/src/Components/ImageHeader/Image-Header-Visualization.js
+++ src/react-first-app/src/Components/ImageHeader/Image-Header-Visualization.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Image-Header-Visualization.css";
+import Image from "../../Resources/Images/Header-Visualization/cheesecake-red.jpg";
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 
 class ImageHeaderVisualization extends React.Component{
     
@@ -9,10 +12,10 @@ class ImageHeaderVisualization extends React.Component{
         return (
             <div className="image-header-visualization">
                 <button className="previous-image">Previous</button>
-                <img src="http://localhost:3000/Resources/Images/Header-Visualization/cheesecake-red.jpg" alt="hola">
-
-                    
-                </img>
+                <AliceCarousel autoPlay autoPlayInterval="3000">
+                    <img src={Image} className="sliderimg" alt=""/>
+                    <img src={Image} className="sliderimg" alt=""/>
+                </AliceCarousel>
                 <button className="next-image">Next</button>
             </div>
         )
